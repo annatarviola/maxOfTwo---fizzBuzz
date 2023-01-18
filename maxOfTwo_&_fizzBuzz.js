2803e07aab103c8ce5f50a50c203e099a491d6f1
Lily Springer <lilyspringer@hotmail.com>
2023-01-18 15:26:56 -0700

    fixed save issue for fizzBuzz solution

--- maxOfTwo_&_fizzBuzz.js
+++ maxOfTwo_&_fizzBuzz.js
@@ -1,3 +1,5 @@
+//CHALLENGE: MAX OF TWO
+
 let x = 1
 let y = 2
 
@@ -9,4 +11,18 @@ if (x > y) {
     console.log(y)
 }  else {
     console.log ("The values are identical")
+}
+
+//CHALLENGE: FIZZ BUZZ
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    } else if (i % 3 === 0) {
+        console.log('fizz')
+    } else if (i % 5 === 0) {
+        console.log('buzz')
+    } else {
+        console.log(i)
+    } 
 }
